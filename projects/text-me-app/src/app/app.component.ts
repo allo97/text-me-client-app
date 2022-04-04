8d96e5317addd62b25c1f15bdc66ec8497bd3eea
Alek <slodczyk97@gmail.com>
2022-04-04 10:51:57 +0200

    initial commit - added prettier - added eslint - created app - added material

---
+++ projects/text-me-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'text-me-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'text-me-app';
+}
